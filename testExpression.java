27e79d41761634336509d9aa5f89e35dd1ae424d
TamCanovic <120271769+TamCanovic@users.noreply.github.com>
2023-05-24 21:53:48 +0200

    Update testExpression.java

--- testExpression.java
+++ testExpression.java
@@ -1,9 +1,9 @@
 @Test
-public void testExpression() {
+public static void testExpression() {
   String expression = "10+5*4+9/3";
-  int expected_result = 33;
-  int actual_result = Calculator.Run(expression);
-  if ( actual_result == expected_result ) {
+  String expected_result = "33";
+  String actual_result = Calculator.Run(expression);
+  if ( actual_result.equals(expected_result) ) {
     System.out.println("Test succeeded");
   else
     System.out.println("Test failed for expression: " + expression);
